560ccf30c2716d570cee80386d002da645a00eb8
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2019-03-07 09:14:14 -0300

    Adds endpoint /accounts/<id>/transactions/

--- kmm/views.py
+++ kmm/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 from kmm.models import Kmmtransactions, Kmmsplits, Kmmpayees, Kmmaccounts
 from kmm.serializers import (
     TransactionSerializer,
@@ -32,3 +36,9 @@ class PayeeViewSet(viewsets.ModelViewSet):
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Kmmaccounts.objects.all()
     serializer_class = AccountSerializer
+
+    @action(detail=True, url_path="transactions", methods=["get"], name="list user groups")
+    def transactions(self, request: Request, pk):
+        transactions = Kmmtransactions.objects.filter(kmmsplits__accountid=pk)
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(data=serializer.data)
